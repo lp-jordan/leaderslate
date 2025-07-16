63aa72749229c8500a35010986ec001b35256b8a
lp-jordan <jordan@leaderpass.com>
2025-07-16 16:40:49 -0400

    Revert "Revert "Add batch note deletion feature""

--- public/script.js
+++ public/script.js
@@ -136,12 +136,17 @@ function exitBatchMode() {
   batchControls.classList.add('hidden');
   document.querySelectorAll('.noteActions').forEach(a => a.classList.remove('hidden'));
   document.querySelectorAll('.noteItem').forEach(n => n.classList.remove('selected'));
+  document.querySelectorAll('.selectBox').forEach(cb => {
+    cb.classList.add('hidden');
+    cb.checked = false;
+  });
 }
 
 function enterBatchMode() {
   batchMode = true;
   batchControls.classList.remove('hidden');
   document.querySelectorAll('.noteActions').forEach(a => a.classList.add('hidden'));
+  document.querySelectorAll('.selectBox').forEach(cb => cb.classList.remove('hidden'));
 }
 
 let editIndex = null;
@@ -368,6 +373,16 @@ function renderNote(note, index) {
   });
   actions.appendChild(delBtn);
 
+  const selectBox = document.createElement('input');
+  selectBox.type = 'checkbox';
+  selectBox.className = 'selectBox hidden';
+  if (selected.has(index)) selectBox.checked = true;
+  selectBox.addEventListener('click', e => {
+    e.stopPropagation();
+    toggleSelection();
+  });
+
+  div.appendChild(selectBox);
   div.appendChild(ts);
   div.appendChild(text);
   div.appendChild(actions);
@@ -376,24 +391,36 @@ function renderNote(note, index) {
 
   if (batchMode) {
     actions.classList.add('hidden');
-  }
-
-  div.addEventListener('click', (e) => {
-if (e.shiftKey && !batchMode) {
-  enterBatchMode();
+if (selectBox) {
+  selectBox.classList.remove('hidden');
 }
 
-if (batchMode) {
+div.addEventListener('click', (e) => {
+  // Enter batch mode if Ctrl (Windows) or Cmd (Mac) is held
+  if ((e.ctrlKey || e.metaKey) && !batchMode) {
+    enterBatchMode();
+  }
+
+  // If in batch mode, toggle selection
+  if (batchMode) {
+    toggleSelection();
+    if (selected.size === 0) exitBatchMode();
+    return;
+  }
+
+  // Otherwise, open the edit modal
+  openEditNoteModal(note, index);
+});
+
+function toggleSelection() {
   if (selected.has(index)) {
     selected.delete(index);
     div.classList.remove('selected');
+    if (selectBox) selectBox.checked = false;
   } else {
     selected.add(index);
     div.classList.add('selected');
+    if (selectBox) selectBox.checked = true;
   }
-  return;
 }
-
-openEditNoteModal(note, index);
-  });
 }
